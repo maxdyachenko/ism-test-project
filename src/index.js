0314fde49f958e26610946e30a199c554e5e3fe3
Maxym Dyachenko <max.dyachenko@inbox.ru>
2018-06-21 13:14:49 +0300

    add dependencies, webpack configuration

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import 'normalize.css';
+// import 'animate.css';
+import './styles/app.styl';
+// import './styles/fonts/roboto.styl';
+// import './styles/fonts/madee.styl';
+// import './styles/fonts/stolzl.styl';
+//
+// import * as c from './js/app.js';
