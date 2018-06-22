f221c34f3d2f4f3074f7335cf71f8d562f208260
Maxym Dyachenko <max.dyachenko@inbox.ru>
2018-06-22 14:41:29 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-
 import './styles/app.styl';
-//import 'bootstrap/dist/css/bootstrap-grid.css';
 import './styles/fonts/lobster13.styl';
 import './styles/fonts/notosans.styl';
 import './styles/fonts/timesnewroman.styl';
-//
-// import * as c from './js/app.js';
+
--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
                 use: [{
                     loader: 'url-loader',
                     options: {
-                        limit: 4000, // Convert images < 8kb to base64 strings
+                        limit: 4000,
                         name: 'fonts/[hash]-[name].[ext]'
                     }
                 }]
