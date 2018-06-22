477e031ef5f40bc91238689e3cde4e460c69bf51
Maxym Dyachenko <max.dyachenko@inbox.ru>
2018-06-22 12:17:55 +0300

    refactoring, add grid, add products section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 import './styles/app.styl';
-import 'bootstrap/dist/css/bootstrap-grid.css';
+//import 'bootstrap/dist/css/bootstrap-grid.css';
 import './styles/fonts/lobster13.styl';
 import './styles/fonts/notosans.styl';
 import './styles/fonts/timesnewroman.styl';
